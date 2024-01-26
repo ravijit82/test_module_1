b7fa71e0d569bfd0cc877c0234a0f92290952638
Ravijeet Udeykar <rudeykar@xceleratorgroup.com>
2024-01-25 20:29:13 -0600

    export routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Button } from "../stories/Button";
 import { Header } from "../stories/Header";
 import { Page } from "../stories/Page";
+import routes from "../stories/Routes";
 
-export { Button, Header, Page };
+export { Button, Header, Page, routes };
